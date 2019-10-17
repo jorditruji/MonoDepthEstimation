2625b50f0eca821c76b74b406b27cee1f9cf15ef
jorditruji <jordimoreratrujillo@gmail.com>
2019-10-17 10:22:58 +0200

    normalization server

--- Data_management/data_normalization.py
+++ Data_management/data_normalization.py
@@ -2,7 +2,7 @@ from dataset import NYUDataset
 import numpy as np
 from torch.utils import data
 import argparse
-
+from tqdm import tqdm
 
 if __name__ == '__main__':
 	# Parse arguments
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
 
 	# Load train partition:
-	train_depth = np.load(args.dataset_partitions).item()['train']
+	train_depth = np.load(args.dataset_partitions, allow_pickle = True).item()['train']
 
 
 	# Create dataset instance and dataloader
@@ -34,11 +34,11 @@ if __name__ == '__main__':
 	stds = []
 	vec_maxs = []
 	vec_mins = []
-	for _idx in range(len(train_depth)):
+	for _idx in tqdm(range(len(train_depth))):
 		# Read and convert to numpies
 		depth, rgb, path = dataset.__getitem__(_idx)
 		depth, rgb = np.array(depth), np.array(rgb,dtype = float)/255.
-		print(depth.shape, rgb.shape)
+		#print(depth.shape, rgb.shape)
 
 		# Calculate interesting data
 		mean = np.mean(rgb, axis = (0,1))
@@ -52,12 +52,15 @@ if __name__ == '__main__':
 		stds.append(std)
 		vec_mins.append(min_depth)
 		vec_maxs.append(max_depth)
-		print("Mean: {} \n STD: {} \n max_depth: {} \n min_depth: {}".format(mean, std, max_depth, min_depth))
+		#print("Mean: {} \n STD: {} \n max_depth: {} \n min_depth: {}".format(mean, std, max_depth, min_depth))
 
 	# Calculate interesting data
 	mean = np.mean(means, axis = 0)
 	std =  np.mean(stds, axis = 0)
 
+	print("mean {}, stds: {}".format(mean, std))
+
+	
 	max_depth = np.max(vec_maxs)
 	min_depth = np.min(vec_mins)
 
