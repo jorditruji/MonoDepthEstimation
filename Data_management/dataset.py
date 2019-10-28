53897f0d1fa2ff5aec625b473b7d7f5a0f45b17b
jorditruji <jordimoreratrujillo@gmail.com>
2019-10-28 11:27:31 +0100

    last bugs

--- Data_management/dataset.py
+++ Data_management/dataset.py
@@ -148,14 +148,14 @@ class NYUDataset(GenericDataset):
 
         """
         depth = self.read_depth(self.depth_frames[index])
-        rgb = read_image(self.RGB_frames[index])
+        rgb = np.asarray(read_image(self.RGB_frames[index]))
 
-        print("Pre augmentation: IMG {}, depth {}".format( rgb.size, depth.shape))
+        print("Pre augmentation: IMG {}, depth {}".format( rgb.shape, depth.shape))
         sample =  {"image": np.array(rgb), "mask": depth}
         augmented = self.transforms(**sample)
         rgb, depth  = augmented['image'], augmented['mask'] 
 
-
+        print("Pst-augmentation: IMG{}, depth{}".format(rgb.shape, depth.shape))
         return depth, rgb, self.depth_frames[index]
 
 
