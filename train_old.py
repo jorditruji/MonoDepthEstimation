1b037e9a37d94469b8aa5a8558e267fc60aa5ba5
jordi_morera_trujillo <jordimoreratrujillo@gmail.com>
2019-11-01 19:29:54 +0100

    WANNA MORE DROPOUT??

--- train_old.py
+++ train_old.py
@@ -25,7 +25,7 @@ from albumentations.pytorch import ToTensor
 import numpy as np
 from albumentations.core.transforms_interface import BasicTransform
 from torch.utils.tensorboard import SummaryWriter
-
+import argparse
 
 class DepthScale(BasicTransform):
     """Transform applied to image only."""
